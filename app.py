2a286bbc70c89dd0336a054eea2dc3dc885b7019
LorenzoLucia <lorenzo.lucia01@gmail.com>
2025-04-29 16:16:04 +0200

    implemented API calls

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from firebase import firebase
-from flask import Flask
+from flask import Flask, request, abort
 
 load_dotenv()
 
@@ -15,9 +15,110 @@ firebase = firebase.FirebaseApplication(FIREBASE_URL, None)
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Rick è un frocio SIIIIIIIIIIII!'
+def get_token(headers):
+    token_id = request.headers.get('auth')
+
+    if token_id is None:
+        abort(401)
+
+    return token_id
+
+
+@app.route('/zone-prices', methods=['GET'])
+def get_zone_prices():
+    token_id = get_token(request.headers)
+    return firebase.get('/zone-prices', None, params={"auth": token_id})
+
+
+@app.route('/payment-methods', methods=['GET'])
+def get_payment_methods():
+    token_id = get_token(request.headers)
+
+    return firebase.get('/payment-methods', None, params={"auth": token_id})
+
+
+@app.route('/tickets', methods=['GET'])
+def get_tickets():
+    token_id = get_token(request.headers)
+
+    return firebase.get('/tickets', None, params={"auth": token_id})
+
+
+@app.route('/profile', methods=['GET'])
+def get_profile():
+    token_id = get_token(request.headers)
+
+    return firebase.get('/profile', None, params={"auth": token_id})
+
+
+@app.route('/plates', methods=['GET'])
+def get_plates():
+    token_id = get_token(request.headers)
+
+    return firebase.get('/plates', None, params={"auth": token_id})
+
+
+@app.route('/users/add-user', methods=['POST'])
+def add_user():
+    token_id = get_token(request.headers)
+
+    body = request.json
+
+    return firebase.post('/users/add-user', body, params={"auth": token_id})
+
+
+@app.route('/users/remove-user', methods=['POST'])
+def remove_user():
+    token_id = get_token(request.headers)
+
+    body = request.json
+
+    return firebase.post('/users/remove-user', body, params={"auth": token_id})
+
+
+@app.route('/users/edit-user', methods=['PUT'])
+def edit_user():
+    token_id = get_token(request.headers)
+
+    body = request.json
+
+    return firebase.put('/users/edit-user', body, params={"auth": token_id})
+
+
+@app.route('/users/add-zone', methods=['POST'])
+def add_zone():
+    token_id = get_token(request.headers)
+
+    body = request.json
+
+    return firebase.post('/zones/add-zone', body, params={"auth": token_id})
+
+
+@app.route('/users/remove-zone', methods=['POST'])
+def remove_zone():
+    token_id = get_token(request.headers)
+
+    body = request.json
+
+    return firebase.post('/zones/remove-zone', body, params={"auth": token_id})
+
+
+@app.route('/users/edit-zone', methods=['PUT'])
+def edit_zone():
+    token_id = get_token(request.headers)
+
+    body = request.json
+
+    return firebase.put('/zones/edit-zone', body, params={"auth": token_id})
+
+
+@app.route('/users/register', methods=['POST'])
+def add_zone():
+    token_id = get_token(request.headers)
+
+    body = request.json
+
+    return firebase.post('/register', body, params={"auth": token_id})
 
 
 if __name__ == '__main__':
