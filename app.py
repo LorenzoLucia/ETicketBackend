5ea43d9dd53b2632bdd65fd034a451adb5e41b16
LorenzoLucia <lorenzo.lucia01@gmail.com>
2025-05-17 11:56:50 +0200

    Implemented some basic API

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
+import uuid
 
 import firebase_admin
 from dotenv import load_dotenv
 from firebase_admin import firestore, credentials, auth
+from firebase_admin.auth import UserNotFoundError
 from flask import Flask, abort, request
 from flask_cors import CORS
 
@@ -19,7 +21,10 @@ firestore_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 app = Flask(__name__)
-
+app.config["API_TITLE"] = "Library API"
+app.config["API_VERSION"] = "v0.0.1"
+app.config["OPENAPI_VERSION"] = "3.1.0"
+app.config["OPENAPI_DESCRIPTION"] = "A simple library API"
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -27,7 +32,10 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def get_firebase_user(token_id):
     decoded_token = auth.verify_id_token(token_id)
     user_id = decoded_token['uid']
-    return auth.get_user(user_id)
+    try:
+        return auth.get_user(user_id)
+    except UserNotFoundError:
+        abort(401)
 
 
 def get_token(headers):
@@ -39,39 +47,57 @@ def get_token(headers):
     return token_id
 
 
-#
-# @app.route('/zone-prices', methods=['GET'])
-# def get_zone_prices():
-#     token_id = get_token(request.headers)
-#     return firebase.get('/zone-prices', None, params={"auth": token_id})
-#
-#
-# @app.route('/payment-methods', methods=['GET'])
-# def get_payment_methods():
-#     token_id = get_token(request.headers)
-#
-#     return firebase.get('/payment-methods', None, params={"auth": token_id})
-#
-#
-# @app.route('/tickets', methods=['GET'])
-# def get_tickets():
-#     token_id = get_token(request.headers)
-#
-#     return firebase.get('/tickets', None, params={"auth": token_id})
-#
-#
-# @app.route('/profile', methods=['GET'])
-# def get_profile():
-#     token_id = get_token(request.headers)
-#
-#     return firebase.get('/profile', None, params={"auth": token_id})
-#
-#
-# @app.route('/plates', methods=['GET'])
-# def get_plates():
-#     token_id = get_token(request.headers)
-#
-#     return firebase.get('/plates', None, params={"auth": token_id})
+def is_user_authenticated(user_id, token_id):
+    db_user = db.collection("users").document(user_id).get().to_dict()
+    firebase_user = get_firebase_user(token_id)
+    return db_user["email"] == firebase_user.email
+
+
+def get_db_user_from_auth(firebase_user):
+    return db.collection("users").where("email", "==", firebase_user.email).get()[0].to_dict()
+
+
+# TODO: validate Firebase Users with user id in the route
+
+@app.route('/zones', methods=['GET'])
+def get_zone_prices():
+    token_id = get_token(request.headers)
+    # This line is only needed to check that the user is authenticated
+    _ = get_firebase_user(token_id)
+    result = db.collection('zones').get()
+    return [i.to_dict() for i in result]
+
+
+@app.route('/users/<user_id>/payment-methods', methods=['GET'])
+def get_payment_methods(user_id: str):
+    token_id = get_token(request.headers)
+    if not is_user_authenticated(user_id, token_id):
+        return abort(401)
+
+    user_payment_methods = db.collection('payment-methods').where("user", "==", user_id).get()
+    return [i.to_dict() for i in user_payment_methods]
+
+
+@app.route('/users/<user_id>/tickets', methods=['GET'])
+def get_tickets(user_id: str):
+    token_id = get_token(request.headers)
+    if not is_user_authenticated(user_id, token_id):
+        return abort(401)
+
+    user_tickets = db.collection('tickets').where("user", "==", user_id).get()
+    return [i.to_dict() for i in user_tickets]
+
+
+@app.route('/users/<user_id>/plates', methods=['GET'])
+def get_plates(user_id: str):
+    token_id = get_token(request.headers)
+    if not is_user_authenticated(user_id, token_id):
+        return abort(401)
+
+    user_plates = db.collection('plates').where("user", "==", user_id).get()
+    return [i.to_dict() for i in user_plates]
+
+
 #
 #
 # @app.route('/users', methods=['POST'])
@@ -101,23 +127,32 @@ def get_token(headers):
 #     return firebase.put('/users/edit-user', body, params={"auth": token_id})
 #
 #
-# @app.route('/zones', methods=['POST'])
-# def add_zone():
-#     token_id = get_token(request.headers)
-#
-#     body = request.json
-#
-#     return firebase.post('/zones/add-zone', body, params={"auth": token_id})
-#
-#
-# @app.route('/zones/<zone_id>', methods=['DELETE'])
-# def remove_zone(zone_id: int):
-#     token_id = get_token(request.headers)
-#
-#     body = request.json
-#
-#     return firebase.post('/zones/remove-zone', body, params={"auth": token_id})
-#
+@app.route('/zones', methods=['POST'])
+def add_zone():
+    token_id = get_token(request.headers)
+
+    user = get_db_user_from_auth(get_firebase_user(token_id))
+    if user["role"] != Role.SYSTEM_ADMINISTRATOR.value or user["role"] != Role.SERVICE_ADMINISTRATOR.value:
+        return abort(401)
+    body = request.json
+
+    uuid4 = str(uuid.uuid4())
+    db.collection('zones').document(uuid4).set(body)
+
+    return db.collection('zones').document(uuid4).get()
+
+
+@app.route('/zones/<zone_id>', methods=['DELETE'])
+def remove_zone(zone_id: str):
+    token_id = get_token(request.headers)
+
+    user = get_db_user_from_auth(get_firebase_user(token_id))
+    if user["role"] != Role.SYSTEM_ADMINISTRATOR.value or user["role"] != Role.SERVICE_ADMINISTRATOR.value:
+        return abort(401)
+    db.collection("zones").document(zone_id).delete()
+    return True
+
+
 #
 # @app.route('/zones/<zone_id>', methods=['PUT'])
 # def edit_zone(zone_id: int):
@@ -128,13 +163,6 @@ def get_token(headers):
 #     return firebase.put('/zones/edit-zone', body, params={"auth": token_id})
 #
 #
-# @app.route('/register', methods=['POST'])
-# def add_zone():
-#     token_id = get_token(request.headers)
-#
-#     body = request.json
-#
-#     return firebase.post('/register', body, params={"auth": token_id})
 
 
 @app.route('/get-me', methods=['GET'])
@@ -142,15 +170,15 @@ def get_me():
     token_id = get_token(request.headers)
     firebase_user = get_firebase_user(token_id)
     email = firebase_user.email
-    query = db.collection("users").where("email", "==", email)
-    result = query.get()
+
+    result = db.collection("users").where("email", "==", email).get()
+
     if len(result) == 0:
         return {
             "is_registered": False,
             "user_data": {}
         }
-    user = query.get()[0]
-    return user.to_dict()
+    return result[0].to_dict()
 
 
 @app.route('/register-user', methods=['POST'])
@@ -160,11 +188,7 @@ def register_user():
     firebase_user = get_firebase_user(token_id)
     email = firebase_user.email
     user = User(name=body["name"], surname=body["surname"], email=email, role=Role.CUSTOMER)
-    db.collection("users").document(user.id).set(user.to_dict())
-    return {
-        "id": user.id,
-        **user.to_dict()
-    }
+    return db.collection("users").document(user.id)
 
 
 if __name__ == '__main__':
